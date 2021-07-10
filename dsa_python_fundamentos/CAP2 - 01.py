0454770e4f72b40e6e69c8d34105d2d7e727f113
Jonh Selmo <jonhsel@gmail.com>
2021-07-10 06:02:37 -0300

    Curso python fundamentos da DSAcademy

---
+++ dsa_python_fundamentos/CAP2 - 01.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#NOTE BOOK JONH SELMO
+
+
+# In[1]:
+
+
+#SOMA
+4+4
+
+
+# In[2]:
+
+
+#MÓDULO
+15%4
+
+
+# In[3]:
+
+
+type(5)
+
+
+# In[4]:
+
+
+type(6.0)
+
+
+# In[5]:
+
+
+j = "my name is Jonh"
+type(j)
+
+
+# #OPERAÇÕES
+
+# In[9]:
+
+
+3.1 + 5.6
+
+
+# In[10]:
+
+
+4 + 4.0
+
+
+# In[14]:
+
+
+B = 4 + 4.0
+print(B, type(B))
+
+
+# In[15]:
+
+
+4/2
+
+
+# In[16]:
+
+
+4//2
+
+
+# In[17]:
+
+
+4//2.0
+
+
+# # conversão
+
+# In[19]:
+
+
+float(9)
+
+
+# In[20]:
+
+
+int(6.0)
+
+
+# In[21]:
+
+
+int(6.5)
+
+
+# In[22]:
+
+
+int(6.6)
+
+
+# In[23]:
+
+
+int(6.9)
+
+
+# In[24]:
+
+
+hex(394)
+
+
+# In[25]:
+
+
+hex(255)
+
+
+# In[26]:
+
+
+bin(255)
+
+
+# In[27]:
+
+
+bin(2)
+
+
+# In[28]:
+
+
+#retorna valor absoluto
+abs(-10)
+
+
+# In[29]:
+
+
+abs(10)
+
+
+# In[36]:
+
+
+round(3.1421, 2)
+
+
+# In[31]:
+
+
+round(6.5)
+
+
+# In[32]:
+
+
+round(6.51)
+
+
+# In[33]:
+
+
+pow(4,2)
+
+
+# In[34]:
+
+
+pow(3,3)
+
+
+# # FIM
---
+++ dsa_python_fundamentos/DSA-Python-Cap01-ComoUtilizarJupyterNotebook.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # <font color='blue'>Data Science Academy - Python Fundamentos</font>
+# # <font color='blue'>Capítulo 1</font>
+# 
+
+# In[ ]:
+
+
+# Versão da Linguagem Python
+from platform import python_version
+print('Versão da Linguagem Python Usada Neste Jupyter Notebook:', python_version())
+
+
+# ## Como Utilizar o Jupyter Notebook
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+2 + 2
+
+
+# In[5]:
+
+
+a = "jonh"
+
+
+# In[6]:
+
+
+b = " selmo"
+
+
+# In[7]:
+
+
+print(a + b)
+
+
+# In[8]:
+
+
+# Pressione as teclas shift e enter para executar esse código e assista o vídeo.
+
+from IPython.display import IFrame
+IFrame(src="https://www.youtube.com/embed/pu-pz77W1P8", width = "560", height = "315")
+
+
+# # Fim
+
+# ### Obrigado - Data Science Academy - <a href="http://facebook.com/dsacademybr">facebook.com/dsacademybr</a>
